56a1d87fc9eeff17a0a390949aa3a9fc46a26316
orangepek0e <georgejohnston13@hotmail.com>
2019-10-19 19:01:15 -0300

    grid container created and filled with placeholder components

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GridContainerComponent } from '../components/grid-container/grid-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridContainerComponent
   ],
   imports: [
     BrowserModule
